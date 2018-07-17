32a59074484bf4daf030e08e26f864821999af89
Austin Doolittle <austindoolittle@gmail.com>
2018-07-16 22:45:09 -0400

    tested curation

--- src/curate.py
+++ src/curate.py
@@ -12,7 +12,8 @@ def sanitize_song_name(post_title):
 	return re.sub(r'(\[.*\]|\(\w+\d+\w+\)+|-)', '', post_title)
 
 def is_song_link(post_link):
-	return 'youtube' in post_link or 'youtu.be' in post_link
+	return 'youtube' in post_link or 'youtu.be' in post_link or \
+			'spotify' in post_link or 'bandcamp' in post_link
 
 def parse_spotify_datetime(datetime_str):
 	return dateutil.parser.parse(datetime_str)
@@ -104,7 +105,7 @@ class PlaylistCurator(object):
 		else:
 			# create the playlist
 			new_playlist = self.spotify.user_playlist_create(self.spotify_username, 
-				expected_playlist_name,
+				playlist_name,
 				public=True)
 			playlist_id = new_playlist['id']
 
@@ -153,38 +154,39 @@ class PlaylistCurator(object):
 		if remove_tracks:
 			self.spotify.user_playlist_remoe_all_occurrences_of_tracks(self.spotify_username, playlist_id, remove_tracks)
 
-	def _add_top_posts(self, subreddit, playlist_id, top_n, time_filter='day'):
+	def _add_top_posts(self, subreddits, playlist_id, top_n):
 		# add top posts from today
 		songs_to_add = []
-		for post in self.reddit.subreddit(subreddit).top(time_filter, limit=None):
-			if not is_song_link(post.url):
-				# this isn't a youtube link, it's fair to assume this isn't a song
-				continue
+		for subreddit in subreddits:
+			for post in self.reddit.subreddit(subreddit).top('day', limit=None):
+				if not is_song_link(post.url):
+					# this isn't a youtube link, it's fair to assume this isn't a song
+					continue
 
-			# sanitize our title and search spotify for a match
-			sanitized_title = sanitize_song_name(post.title)
-			search_result = self.spotify.search(q=sanitized_title, type='track')
+				# sanitize our title and search spotify for a match
+				sanitized_title = sanitize_song_name(post.title)
+				search_result = self.spotify.search(q=sanitized_title, type='track')
 
-			# check the case that no valid items were returnd
-			if len(search_result['tracks']['items']) == 0:
-				continue
+				# check the case that no valid items were returnd
+				if len(search_result['tracks']['items']) == 0:
+					continue
 
-			# get the first result and add it to our playlist
-			songs_to_add.append(search_result['tracks']['items'][0]['id'])
-			if len(songs_to_add) == top_n:
-				break
+				# get the first result and add it to our playlist
+				songs_to_add.append(search_result['tracks']['items'][0]['id'])
+				if len(songs_to_add) == top_n:
+					break
 
-		if songs_to_add:
-			self.spotify.user_playlist_add_tracks(self.spotify_username, playlist_id, songs_to_add)
+			if songs_to_add:
+				self.spotify.user_playlist_add_tracks(self.spotify_username, playlist_id, songs_to_add)
 
 
-	def curate_playlist(self):
+	def curate(self):
 		#clear out songs that are currently in the playlist that have been there longer than *expire_days*
 		for playlist_id, config_dict in self._config_dict.items():
 			if config_dict['expire_days'] >= 0:
 				self._trim_expired_from_playlist(playlist_id, config_dict['expire_days'])
 
 			# add new songs
-			self._add_top_posts(config_dict['subreddits'], playlist_id, config_dict['top_n'], config_dict['time_filter'])
+			self._add_top_posts(config_dict['subreddits'], playlist_id, config_dict['top_n'])
 
 
